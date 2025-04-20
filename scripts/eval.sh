71e6774b5c67670d493ac1cb659dfd1b7e10a264
Pranav Sharma <122373452+kartyg23@users.noreply.github.com>
2025-04-21 02:40:19 +0530

    changed batch size to 16

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -4,13 +4,12 @@ PY_ARGS=${@:1} # Any other arguments
 
 python main_finetune.py \
   --model AIDE \
-  --batch_size 32 \
   --blr 5e-4 \
   --epochs 5 \
   --device cuda:0 \
   --eval True \
   --eval_data_path /content/drive/MyDrive/Real_fake_dataset \
-  --batch_size 6 \
+  --batch_size 16 \
   --num_workers 0 \
   --resume /content/drive/MyDrive/model/sd14_train.pth \
   --output_dir results/sd14_train \
