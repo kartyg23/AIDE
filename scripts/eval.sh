8bd88fa4d71a926584bddc71c96342df7582bff2
Pranav Sharma <122373452+kartyg23@users.noreply.github.com>
2025-04-21 02:58:52 +0530

    changed path to kaggle paths

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -8,9 +8,9 @@ python main_finetune.py \
   --epochs 5 \
   --device cuda:0 \
   --eval True \
-  --eval_data_path /content/drive/MyDrive/Real_fake_dataset \
+  --eval_data_path /kaggle/input/real-fake-dataset/Real_fake_dataset \
   --batch_size 16 \
   --num_workers 0 \
-  --resume /content/drive/MyDrive/model/sd14_train.pth \
+  --resume /kaggle/input/model/sd14_train.pth \
   --output_dir results/sd14_train \
   ${PY_ARGS}
